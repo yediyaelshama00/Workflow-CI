c7ac251d43aaa635d3d2e554dfcf08aea0aa129f
unknown <yediya061194@gmail.com>
2025-12-24 10:43:24 +0700

    Final Fix: Manual logging and robust ID discovery

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,20 +5,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
-# 1. Autologging MLflow 
-mlflow.sklearn.autolog()
-
-# 2. Membaca Data
+# 1. Membaca Data
 df = pd.read_csv('diabetes_preprocessed.csv')
 X = df.drop('Outcome', axis=1)
 y = df['Outcome']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. Menjalankan Training
+# 2. Menjalankan Training
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
-    
+
+# 3. Simpan Model Secara Manual
+# Memastikan folder 'artifacts/model' dibuat oleh MLflow
+mlflow.sklearn.log_model(model, "model")
+
 # 4. Verifikasi di log GitHub Actions
 y_pred = model.predict(X_test)
 report = classification_report(y_test, y_pred)
